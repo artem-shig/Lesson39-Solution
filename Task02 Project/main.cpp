f136c656a4cd34d73820dc925b680a216fc47574
user <user@WIN-5FIO8HJUO88>
2025-07-03 23:55:06 +0300

    Update Task02(The Perfect Number).

--- Task02 Project/main.cpp
+++ Task02 Project/main.cpp
@@ -5,8 +5,11 @@ int main() {
 	cout << "The Perfect Number.\n";
 
 	int number;
-	cout << "Enter your number: ";
-	cin >> number;
+
+	do {
+		cout << "Enter your number: ";
+		cin >> number;
+	} while (number % 2 != 0);
 
 	string msg = is_perfect_number(number) ? "is" : "is not";
 
